645bfc78739bfc588271a2199844c007ed76458a
nick.atkocaitis <n.atkocaitis@gmail.com>
2021-02-03 22:53:30 -0500

    Figuring out how to get the new input system working

--- Assets/Src/Network/Network.cs
+++ Assets/Src/Network/Network.cs
@@ -1,27 +1,10 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 using Mirror;
 
 public class Network : NetworkBehaviour
 {
     void Update()
     {
-        if (isLocalPlayer)
-        {
-            Keyboard keyboard = Keyboard.current;
-            if (keyboard == null)
-            {
-                return;
-            }
 
-
-            int horizontalMove = 0;
-            if (keyboard.IsPressed((float)Key.A))
-            {
-                horizontalMove = -1;
-            }
-            Vector3 movement = new Vector3(horizontalMove, 0, 0);
-            transform.position = transform.position + movement;
-        }
     }
 }
--- Assets/Src/Player/MovementSystems/FirstPersonInfantryMove.cs
+++ Assets/Src/Player/MovementSystems/FirstPersonInfantryMove.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Mirror;
 
 public class FirstPersonInfantryMove : MonoBehaviour
 {
+    double MoveSpeed = 0.1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void MoveAction(InputAction.CallbackContext context)
+    {
+        // if (isLocalPlayer)
+        // {
+        Debug.Log("Here");
+        Keyboard keyboard = Keyboard.current;
+        if (keyboard == null)
+        {
+            return;
+        }
+
+
+        int horizontalMove = 0;
+        if (keyboard.IsPressed((float)Key.A))
+        {
+            horizontalMove = -1;
+        }
+        Vector3 movement = new Vector3(horizontalMove, 0, 0);
+        transform.position = transform.position + movement;
+        // }
+
+        Debug.Log(context.ToString());
     }
 }
