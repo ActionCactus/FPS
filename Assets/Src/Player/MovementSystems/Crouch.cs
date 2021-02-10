6ae41e4e083890cb1d2cf74ec3434f484a0a3ca5
nick.atkocaitis <n.atkocaitis@gmail.com>
2021-02-10 18:15:57 -0500

    Made crouch reduce player speed

--- Assets/Src/Player/MovementSystems/Crouch.cs
+++ Assets/Src/Player/MovementSystems/Crouch.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using FPS.Player.MovementSystems;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(MoveSystem))]
 public class Crouch : MonoBehaviour
 {
+    // TODO: because this is so dumb, and does the same thing sprint does, sprinting while crouched will
+    // mess up the move speed
     public InputAction InputAction;
+    public float moveSpeedReductionMultiplier = 0.5f;
 
     private Collider playerCollider;
     private Transform playerTransform;
+    private MoveSystem playerMoveSystem;
     private bool isCrouched;
+    private float originalMoveSpeed;
+
 
     public void Start()
     {
         this.playerCollider = this.GetComponent<Collider>();
         this.playerTransform = this.GetComponent<Transform>();
+        this.playerMoveSystem = this.GetComponent<MoveSystem>();
+        this.originalMoveSpeed = this.playerMoveSystem.MoveSpeedMultiplier;
         this.InputAction.Enable();
     }
 
@@ -32,6 +42,8 @@ public class Crouch : MonoBehaviour
                 this.playerTransform.transform.localScale.y / 2,
                 this.playerTransform.transform.localScale.z
             );
+            this.originalMoveSpeed = this.playerMoveSystem.MoveSpeedMultiplier;
+            this.playerMoveSystem.MoveSpeedMultiplier = this.playerMoveSystem.MoveSpeedMultiplier * this.moveSpeedReductionMultiplier;
             this.isCrouched = true;
         }
         else if (this.isCrouched && !this.InputAction.IsPressed())
@@ -47,6 +59,7 @@ public class Crouch : MonoBehaviour
                 this.playerTransform.transform.localScale.y * 2,
                 this.playerTransform.transform.localScale.z
             );
+            this.playerMoveSystem.MoveSpeedMultiplier = this.originalMoveSpeed;
             this.isCrouched = false;
         }
     }
