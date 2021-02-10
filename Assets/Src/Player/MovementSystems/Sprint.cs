8c803673bf5b09ff754d5ec686391e9d9879418f
nick.atkocaitis <n.atkocaitis@gmail.com>
2021-02-10 00:16:55 -0500

    Cleanup

--- Assets/Src/Player/MovementSystems/Sprint.cs
+++ Assets/Src/Player/MovementSystems/Sprint.cs
@@ -13,7 +13,7 @@ public class Sprint : MonoBehaviour
     private bool isSprinting = false;
 
 
-    void Start()
+    public void Start()
     {
         this.playerMovementSystem = GetComponent<MoveSystem>();
         this.movementSystemType = this.playerMovementSystem.GetType();
@@ -22,7 +22,7 @@ public class Sprint : MonoBehaviour
         this.sprintButton.Enable();
     }
 
-    void Update()
+    public void Update()
     {
         this.currentMoveSystemType = GetComponent<MoveSystem>().GetType();
         if (this.movementSystemType != this.currentMoveSystemType)
@@ -38,13 +38,11 @@ public class Sprint : MonoBehaviour
         // Don't need a null check because of the RequireComponent attribute
         if (!this.isSprinting && this.sprintButton.IsPressed())
         {
-            Debug.Log("Pressed!");
             this.playerMovementSystem.MoveSpeedMultiplier = this.playerMovementSystem.MoveSpeedMultiplier * 2;
             this.isSprinting = true;
         }
         else if (this.isSprinting && !this.sprintButton.IsPressed())
         {
-            Debug.Log("Released!");
             this.playerMovementSystem.MoveSpeedMultiplier = this.originalMoveSpeed; // division is apparently expensive?  We'll save a tick or two here lol
             this.isSprinting = false;
         }
